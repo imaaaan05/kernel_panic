1eb38c69900c361fd025b387b724d57b49afede7
Dídac <104131138+Lopsidedship9@users.noreply.github.com>
2026-05-09 18:40:56 +0200

    clean database done

--- smart-demand-signals/clean_data.py
+++ smart-demand-signals/clean_data.py
@@ -13,27 +13,35 @@ ventas = ventas[ventas["Valores_H"] != 0]
 ventas = ventas[ventas["Unidades"] != 0]
 ventas = ventas.drop(columns=["Num.Fact"])
 
-print("\nVentas merge")
-print(ventas.head())
 
-print("\nVentas merge")
-print(productos.head())
 
-productos = productos.rename(columns={
-    "Id.Prod": "Id. Producto"
-})
+productos = productos.rename(columns={"Id.Prod": "Id. Producto"})
+
+print(potencial.columns.tolist())
 
 ventas_v2 = ventas.merge(    
     productos[
-        ["Id. Producto", "Familia_H"]
+        ["Id. Producto", "Familia_H", "Categoria_H"]
     ],
     on="Id. Producto",
     how="left"
     )
 
+ventas_v3 = ventas_v2.rename(columns={"Categoria_H": "Categoria Productos", "Id. Cliente": "Id.Cliente"})
+
+ventas_v3 = ventas_v3.merge(
+    potencial[["Id.Cliente", "Categoria Productos", "Potencial_H"]],
+    on=["Id.Cliente", "Categoria Productos"],
+    how="left"
+)
+
+print()
 
 print("\nVentas merge")
-print(ventas_v2.head())
+print(ventas_v3.head())
 
 print("\n Id = 26")
-print(ventas_v2[ventas_v2["Id. Cliente"] == 26].sort_values("Fecha"))
+print(ventas_v3[ventas_v3["Id.Cliente"] == 26].sort_values("Fecha"))
+ventas_v3.drop(columns=["Categoria Productos"], inplace=True)
+
+ventas_v3.to_excel("data/database_clean.xlsx", index=False)
